55870fda728db84cd94800d106c1a92b54df5f93
Jenny Ly <jtly102@gmail.com>
2026-05-28 23:40:08 -0700

    finished diameter

--- graph_algorithms.py
+++ graph_algorithms.py
@@ -8,13 +8,60 @@
 #                            the keys are the degree, and the values is the number of nodes with that
 #                            degree.
 
-
 from graph import Graph
+import random
+from collections import deque
+
+'''
+first bfs (bfs random) = random node to get the furthest node 
+second bfs (def bfs_max_distance, calls bfs )= gets the max distance 
+
+bfs_random = uses random num gen; inputs random num to bfs; returns [node, distance]
+bfs = uses node; returns [node, distance]
+bfs_max_distance = calls bfs; uses node to find max distance; returns distance
+get_diameter = calls bfs_max_distance a few times, then picks the max
+'''
+
+# [int, int] being [node, distance]
+def bfs_random(graph:Graph) -> list[int, int]:
+	random_num = random.randint(0, graph.get_num_nodes()-1)
+	return bfs(graph, random_num)
+	
+# [int, int] being [node, distance]
+def bfs(graph:Graph, node:int) -> list[int, int]:
+	deq = deque([node])
+	seen = set([node])
+	distance = {}
+	distance[node] = 0
+
+	while deq:
+		cur = deq.popleft()
+		
+		neighbors = graph.get_neighbors(cur)
+		for neighbor in neighbors:
+			if neighbor not in seen:
+				seen.add(neighbor)
+				deq.append(neighbor)
+				distance[neighbor] = distance[cur] + 1
+	furthest_node = max(distance, key=distance.get)
+	dist = distance[furthest_node]
+	return [furthest_node, dist]
+
+# returns distance
+def bfs_max_distance(graph:Graph) -> int:
+	bfs1 = bfs_random(graph)
+	dis1 = bfs1[1]
+	bfs2 = bfs(graph, bfs1[0])
+	dis2 = bfs2[1]
+
+	return max(dis1, dis2)
 
 def get_diameter(graph: Graph) -> int:
-	raise NotImplementedError
-
-
+	if graph.get_num_nodes() < 10:
+		return bfs_max_distance(graph)
+	else:
+		return max(bfs_max_distance(graph), bfs_max_distance(graph), bfs_max_distance(graph))
+	
 def get_clustering_coefficient(graph: Graph) -> float:
 	raise NotImplementedError
 
